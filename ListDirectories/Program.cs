82b0cadb2a0ed85c73a639cc7f76f7c1801a039b
AYZBTR <101973630+AYZBTR@users.noreply.github.com>
2022-04-30 19:01:27 +0200

    Add files via upload

---
+++ ListDirectories/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace ListDirectories
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var files = Directory.GetFiles(Directory.GetCurrentDirectory());
+            var directories = Directory.GetDirectories(Directory.GetCurrentDirectory());
+            string change = Convert.ToString(Directory.GetCurrentDirectory());
+            change += '\\';
+            foreach (var dir in directories)
+            {
+                Console.WriteLine(dir);
+            };
+
+            foreach (var file in files)
+            {
+                Console.WriteLine(file.Replace(change, " "));
+            }
+        }
+    }
+}
---
+++ WordCount/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace WordCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pathoffile = args[0];
+            StreamReader sr = new StreamReader(pathoffile);
+            int counter = 0;
+            string delim = " ,.";
+            List<string> fields = new List<string>();
+            string line = null;
+
+            while (!sr.EndOfStream)
+            {
+                line = sr.ReadLine();
+                counter++;
+                fields.AddRange(line.Split(delim.ToCharArray()));
+            }
+            sr.Close();
+            Console.WriteLine("The word count is {0}", fields.Count);
+            Console.WriteLine("The Line count is {0}", counter);
+        }
+    }
+}
+           
+        
+
+
+
